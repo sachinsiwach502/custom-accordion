384d5be8c4b982ad36ad740207678585cfba2e51
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-02-23 12:31:42 +0530

    accodian

--- assets/ja/app.js
+++ assets/ja/app.js
@@ -1,25 +1,24 @@
-let icon = document.querySelector("#svg");
-icon.addEventListener("click", () => {
+let acord_item = document.querySelectorAll(".accord-part");
 
+acord_item.forEach(element => {
+    let box = element.querySelector("#box");
+    let icon = element.querySelector("#svg");
+    let text = element.querySelector(".txt");
+    let full_box = element.querySelector("#full_box");
     let num = 0;
-    if (num < 1) {
-        icon.style.transform = "rotate(180deg)";
-        console.log("if")
-        return num = 1;
-    } else if (num === 1) {
-        icon.style.transform = "rotate(0deg)";
-        console.log("else")
-        return num = 0;
-    }
+
+    element.addEventListener("click", () => {
+        if (num === 0) {
+            icon.style.transform = "rotate(180deg)";
+            text.style.display = "none";
+            num = 1;
+        } else {
+            icon.style.transform = "rotate(0deg)";
+            text.style.display = "block";
+            num = 0;
+        }
+    });
 });
 
-// let icon = document.querySelector("#svg");
-// icon.addEventListener("click", () => {
-//     let value = icon.style.transform;
-//     if (value === "none") {
-//         value = "rotate(180deg)";
-//     } else {
-//         value = "rotate(0deg)";
-//     }
-//     icon.style.transform = value;
-// });
+
+
