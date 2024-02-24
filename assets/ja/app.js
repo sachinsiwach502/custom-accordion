b2d1f96cd6329e6856e8eae7df6e5e3393fd3eac
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-02-24 09:37:46 +0530

    accodien

--- assets/ja/app.js
+++ assets/ja/app.js
@@ -1,24 +1,48 @@
-let acord_item = document.querySelectorAll(".accord-part");
+let acord_items = document.querySelectorAll(".accord-part");
+let acord_item = document.querySelector(".accord-part>p");
 
-acord_item.forEach(element => {
+let full_box = document.querySelector("#full_box");
+let box = document.querySelector("#box");
+let text = document.querySelector(".txt");
+let icon = document.querySelector("#svg");
+
+full_box.addEventListener("click", () => {
+    box.classList.toggle("active");
+
+})
+
+acord_item.style.display = "block";
+icon.style.transform = "rotate(180deg)";
+
+acord_items.forEach(element => {
     let box = element.querySelector("#box");
-    let icon = element.querySelector("#svg");
     let text = element.querySelector(".txt");
+    let icon = element.querySelector("#svg");
     let full_box = element.querySelector("#full_box");
     let num = 0;
 
+
     element.addEventListener("click", () => {
+        acord_items.forEach(otherelement => {
+            if (otherelement !== element) {
+                let svg = otherelement.querySelector("#svg");
+                let disp_text = otherelement.querySelector(".txt");
+                disp_text.style.display = "none";
+                svg.style.transform = "rotate(0deg)"
+            }
+        });
+
+
         if (num === 0) {
-            icon.style.transform = "rotate(180deg)";
             text.style.display = "none";
+            icon.style.transform = "rotate(0deg)";
+            box.classList.remove("active");
             num = 1;
         } else {
-            icon.style.transform = "rotate(0deg)";
             text.style.display = "block";
+            icon.style.transform = "rotate(180deg)";
             num = 0;
+            box.classList.add("active");
         }
     });
 });
-
-
-
